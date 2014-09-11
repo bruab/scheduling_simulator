fa9dd7a341673cb9fd0edf72fbf7cc70ed54596a
Brian Hall <brian@reallymany.com>
2014-09-10 21:44:42 -1000

    my first ifndef; can now compile with flag

--- assignment_1/exercise1.c
+++ assignment_1/exercise1.c
@@ -9,7 +9,9 @@
 // 		4 - j, k, i
 // 		5 - k, i, j
 // 		6 - k, j, i
-# define ORDER 2
+#ifndef ORDER
+# define ORDER 1
+#endif
 
 // Set row/column size
 // 1100 is the magic number on my crappy laptop
@@ -37,13 +39,8 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	// Multiply A x B
-// Orders are: 	1 - i, j, k
-// 		2 - i, k, j
-// 		3 - j, i, k
-// 		4 - j, k, i
-// 		5 - k, i, j
-// 		6 - k, j, i
+// Multiply A x B
+// 1 - i, j, k
 #if ORDER == 1
 	gettimeofday(&t0, NULL);
 	for (i=0; i<N; i++) {
@@ -55,11 +52,65 @@ int main(int argc, char *argv[])
 	}
 	gettimeofday(&t1, NULL);
 	elapsedTime = t1.tv_sec - t0.tv_sec;
+	
+// 2 - i, k, j
 #elif ORDER == 2
 	gettimeofday(&t0, NULL);
 	for (i=0; i<N; i++) {
 		for (k=0; k<N; k++) {
 			for (j=0; j<N; j++) {
+				C[i][j] = C[i][j] + (A[i][k] * B[k][j]);
+			}
+		}
+	}
+	gettimeofday(&t1, NULL);
+	elapsedTime = t1.tv_sec - t0.tv_sec;
+
+// 3 - j, i, k
+#elif ORDER == 3
+	gettimeofday(&t0, NULL);
+	for (j=0; j<N; j++) {
+		for (i=0; i<N; i++) {
+			for (k=0; k<N; k++) {
+				C[i][j] = C[i][j] + (A[i][k] * B[k][j]);
+			}
+		}
+	}
+	gettimeofday(&t1, NULL);
+	elapsedTime = t1.tv_sec - t0.tv_sec;
+
+// 4 - j, k, i
+#elif ORDER == 4
+	gettimeofday(&t0, NULL);
+	for (j=0; j<N; j++) {
+		for (k=0; k<N; k++) {
+			for (i=0; i<N; i++) {
+				C[i][j] = C[i][j] + (A[i][k] * B[k][j]);
+			}
+		}
+	}
+	gettimeofday(&t1, NULL);
+	elapsedTime = t1.tv_sec - t0.tv_sec;
+
+// 5 - k, i, j
+#elif ORDER == 5
+	gettimeofday(&t0, NULL);
+	for (k=0; k<N; k++) {
+		for (i=0; i<N; i++) {
+			for (j=0; j<N; j++) {
+				C[i][j] = C[i][j] + (A[i][k] * B[k][j]);
+			}
+		}
+	}
+	gettimeofday(&t1, NULL);
+	elapsedTime = t1.tv_sec - t0.tv_sec;
+
+// 6 - k, j, i
+#elif ORDER == 6
+	gettimeofday(&t0, NULL);
+	for (k=0; k<N; k++) {
+		for (j=0; j<N; j++) {
+			for (i=0; i<N; i++) {
 				C[i][j] = C[i][j] + (A[i][k] * B[k][j]);
 			}
 		}
