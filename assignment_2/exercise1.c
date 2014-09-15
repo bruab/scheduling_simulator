5c2e1f5cd2bfa3d6a0a9d4770089c348b82c5c39
Brian Hall <brian@reallymany.com>
2014-09-15 06:58:21 -1000

    set up assignment2

---
+++ assignment_2/exercise1.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+// Set row/column size
+// 1100 is the magic number on my crappy laptop
+#define N 1100
+
+// Declare arrays
+int A[N][N];
+int B[N][N];
+int C[N][N];
+
+int main(int argc, char *argv[])
+{
+	int i, j, k, sum;
+	struct timeval t0, t1;
+	double elapsedTime;
+
+	// Populate A and B with random integers
+	int seed = 1234;
+	srand(seed);
+	for (i=0; i<N; i++) {
+		for (j=0; j<N; j++) {
+			A[i][j] = rand();
+			B[i][j] = rand();
+			C[i][j] = 0;
+		}
+	}
+
+	// Multiply A x B
+	char ordering[] = {'i', 'k', 'j', '\0'};
+	gettimeofday(&t0, NULL);
+	for (i=0; i<N; i++) {
+		for (k=0; k<N; k++) {
+			for (j=0; j<N; j++) {
+				C[i][j] = C[i][j] + (A[i][k] * B[k][j]);
+			}
+		}
+	}
+	gettimeofday(&t1, NULL);
+	elapsedTime = t1.tv_sec - t0.tv_sec;
+
+	// Output diagonal sum of C
+	sum = 0;
+	for (i=0; i<N; i++) {
+		sum = sum + C[i][i];
+	}
+
+	printf("%d\t%f\t%s\n", sum, elapsedTime, ordering);
+
+	return 0;
+}
+
---
+++ assignment_2/exercise2_startingpoint.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <math.h>
+#include <unistd.h>
+#include <signal.h>
+
+#define N 12000
+
+int A[N+2][N+2];
+
+int main(int argc, char **argv) {
+
+  int num_threads;
+  int num_iterations;
+
+  int iter;
+  int i,j;
+  struct timeval start, end;
+
+  if (argc != 3) {
+	  fprintf(stderr,"Usage: %s <num threads> <num iterations>\n", argv[0]);
+	  exit(1);
+  }
+
+  if (sscanf(argv[1],"%d",&num_threads) != 1) {
+	  fprintf(stderr,"Invalid number of threads\n");
+	  exit(1);
+  }
+
+  if (sscanf(argv[2],"%d",&num_iterations) != 1) {
+	  fprintf(stderr,"Invalid number of iterations\n");
+	  exit(1);
+  }
+
+  // Fill in the array
+  for (i=0; i < N+2; i++) {
+    for (j=0; j < N+2; j++) {
+      A[i][j] = i+j;
+    }
+  }
+
+  // Start timer
+  gettimeofday(&start,NULL);
+
+  // Loop for num_iterations iterations
+  for (iter = 0; iter < num_iterations; iter++) {
+    fprintf(stderr,"."); fflush(stderr);
+
+    for (i = 1; i < N+1; i++) {
+      for (j = 1; j < N+1; j++) {
+        A[i][j] = (3*A[i-1][j] + A[i+1][j] + 3*A[i][j-1] + A[i][j+1])/4;
+      }
+    }
+  }
+
+  gettimeofday(&end,NULL);
+  printf("\n%f\n", 
+		 (end.tv_sec*1000000.0 + end.tv_usec - 
+		  start.tv_sec*1000000.0 - start.tv_usec) / 1000000.0);
+
+  // Compute and print the sum of elements for correctness checking
+  int sum =0 ;
+  for (i=1; i < N; i++) {
+    for (j=1; j < N; j++) {
+      sum += A[i][j];
+    }
+  }
+  fprintf(stderr,"sum = %d\n",sum);
+
+  exit(0);
+}
