6f138a7f2f1298e2fbf1236009d92ceaaa242d02
Brian Hall <brian@reallymany.com>
2014-10-10 08:08:54 -1000

    made more host files, added to build script

--- assignment_3/build_all_executables.sh
+++ assignment_3/build_all_executables.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+# Remove any executables that might be hanging around
+if [[ -f question1 ]]
+then
+	rm question1
+fi
+
+if [[ -f question1_naive ]]
+then
+	rm question1_naive
+fi
+
 smpicc question1.c -DVERSION=default -o question1
 smpicc question1.c -DVERSION=naive -o question1_naive
