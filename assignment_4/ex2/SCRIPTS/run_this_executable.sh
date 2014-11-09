5542216de7ab8db7710df078f8a0fa7a16cf1cd3
Brian Hall <brian@reallymany.com>
2014-11-09 09:42:03 -1000

    made robuster run script

--- assignment_4/ex2/SCRIPTS/run_this_executable.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-smpirun --cfg=smpi/bcast:mpich --cfg=smpi/running_power:0.007 -np 4 -platform xml_and_host_files/1600_hosts/cluster_1600.xml -hostfile xml_and_host_files/1600_hosts/hostfile_1600 ./$@
-
-
